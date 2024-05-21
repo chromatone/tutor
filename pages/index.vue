1a7311250bb34a1d27f62ddac3147c1f1e29a5be
davay <davay@defucc.me>
2024-05-21 23:10:42 +0700

    share

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <script setup>
+import { useShare } from '@vueuse/core'
 
+const { share, isSupported } = useShare()
+
+function startShare() {
+  share({
+    title: 'Denis Starov',
+    text: 'Chromatone tutor',
+    url: 'https://tutor.chromatone.center/',
+  })
+}
+
+import { renderSVG } from 'uqr'
+
+const svg = renderSVG('https://tutor.chromatone.center/', {
+  border: 3,
+  blackColor: '#000b',
+  ecc: 'M'
+})
 </script>
 
 <template lang="pug">
@@ -26,28 +44,33 @@
     .text-lg.bg-light-400.p-6.rounded-2xl.shadow-xl.flex.flex-col.gap-4(style="flex: 1 1 300px")
       p If you find it interesting and useful in your own musical journey - let's collaborate! I'm ready to share my experience and knowledge in dense and complete form of 1-hour lessons according to personal program developed on the first orientational meeting. 
 
-    .flex.flex-wrap.gap-8
+    .flex.flex-wrap.gap-8(style="flex: 1 1 200px")
       .flex-1.flex.gap-8.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-light-300.text-4xl Let's talk!
-      a.flex-1.flex.gap-4.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-light-200(
+      a.flex-1.flex.gap-4.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-dark-200.text-light(
         href="https://discordapp.com/users/535370860521979914/" 
         target="_blank"
         )
         .i-la-discord.text-4xl
         .text-2xl davay42
 
-      a.flex-1.flex.gap-4.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-light-200(
+      a.flex-1.flex.gap-4.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-dark-200.text-light(
         href="https://t.me/starov"
         target="_blank"
         )
         .i-la-telegram.text-4xl
         .text-2xl starov
 
-      a.flex-1.flex.gap-4.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-light-200(
+      a.flex-1.flex.gap-4.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-dark-200.text-light(
         href="https://instagram.com/starov"
         target="_blank"
         )
         .i-la-instagram.text-4xl
         .text-2xl starov
+    .flex.flex-wrap.gap-4.w-full.items-start.flex-1
+      button.flex.items-center.gap-4.p-4.bg-dark-200.text-light.shadow-xl.rounded-2xl(v-if="isSupported" @click="startShare()")
+        .i-la-share.text-4xl
+        .text-2xl Share this page
+      .rounded-2xl.overflow-clip.z-10.max-w-45ch(style="flex: 1 1 240px" v-html="svg")
 </template>
 
 <style scoped lang="postcss"></style>
