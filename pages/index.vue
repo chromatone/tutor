385fa7d10f92586b8b2c437fcfbaca634c3fd8b1
davay <davay@defucc.me>
2024-05-25 12:24:00 +0700

    better page

--- pages/index.vue
+++ pages/index.vue
@@ -48,7 +48,7 @@ async function pay() {
 </script>
 
 <template lang="pug">
-.flex.flex-wrap.items-start.gap-8.p-8( style="background:  linear-gradient(hsla(300deg,1%,90%,0.2) 0%,hsla(300deg,1%,100%,0.7) 20%, hsla(0deg,50%,100%,0.9) 100%), url(/pattern.jpg); background-size: 700px; background-attachment: normal; background-repeat: all")
+.flex.flex-wrap.items-start.gap-8.p-8( style="background:  linear-gradient(hsla(300deg,1%,90%,0.9) 0%,hsla(300deg,1%,100%,0.7) 50%, hsla(0deg,50%,100%,0.2) 100%), url(/pattern.jpg); background-size: 700px; background-attachment: normal; background-repeat: all")
   .flex.flex-col.gap-8.z-2(style="flex: 1 1 45ch")
     a.flex.items-center.gap-4.bg-light-400.p-4.rounded-2xl.shadow-xl(href="https://chromatone.center" target="_blank")
       img.w-10.z-2(src="~assets/images/logo.svg")
@@ -65,26 +65,30 @@ async function pay() {
         h2.text-xl Phuket, Thailand
   .flex.flex-wrap.gap-8(style="flex: 1 1 45ch")
     .bg-light-400.p-6.rounded-2xl.shadow-xl.flex.flex-col.gap-4(style="flex: 1 1 300px")
-      p I'm exploring new ways to interconnect senses and illuminate hidden depths of music and arts. More than 7-years long Chromatone odyssey brings me wide musician experience, deep music theory understanding and a handful of open-source interactive web apps to study, play and perform with. Now I'm ready to help you on your way in music. Here are my main subjects:
+      p I'm developing new ways to interconnect senses and illuminate hidden depths of music and arts. Through my 7+ years long Chromatone odyssey I got wide musician experience, deep music theory understanding and a handful of open-source interactive web apps to teach, play and perform with. Now I'm ready to help you on your way in music with personalized programs tailored for your goals and inspirations.  Here are my main subjects:
     .flex.flex-wrap.gap-8(style="flex: 1 1 45ch")
 
       .flex.flex-col.gap-2.p-4.bg-light-400.rounded-2xl.shadow-xl(style="flex: 1 1 40%")
         h3.text-2xl Visual Music Theory
-      .p-4.bg-light-400.rounded-2xl.shadow-xl(style="flex: 1 1 40%")
+        .text-sm Understand notes, intervals, rhythms, chords and more
+      .flex.flex-col.gap-2.p-4.bg-light-400.rounded-2xl.shadow-xl(style="flex: 1 1 40%")
         h3.text-2xl Collaborative performance
-      .p-4.bg-light-400.rounded-2xl.shadow-xl(style="flex: 1 1 40%")
+        .text-sm Learn how to improvise with others and how to organize jams
+      .flex.flex-col.gap-2.p-4.bg-light-400.rounded-2xl.shadow-xl(style="flex: 1 1 40%")
         h3.text-2xl Live Multimedia Production
-      .p-4.bg-light-400.rounded-2xl.shadow-xl(style="flex: 1 1 40%")
+        .text-sm Build your own portable setup to create music live
+      .flex.flex-col.gap-2.p-4.bg-light-400.rounded-2xl.shadow-xl(style="flex: 1 1 40%")
         h3.text-2xl Creative Web Development
-
+        .text-sm Use JavaScript to create stunning interactive web-apps
 
   .flex.flex-col.gap-4(style="flex: 1 1 300px")
     .bg-light-200.p-4.rounded-2xl.shadow-xl.flex.flex-col.gap-4
       .text-2xl Private tutorship session
       .flex.flex-wrap.gap-2.items-baseline.mx-2
-        .text-4xl $45
-        .flex-1.text-center.text-xl.op-50 or
-        .text-3xl.op-50(title="1500 THB - Thai Baht") ฿1500
+        .flex.items-baseline
+          .text-4xl $45
+          .text-2xl.op-50 /hr
+        .text-2xl.op-50(title="1500 THB - Thai Baht") (฿1500/hr)
       ul.list-circle.list-inside.my-2
         li 1 hour one-on-one session
         li Online video call (Google Meet) 
@@ -107,12 +111,12 @@ async function pay() {
         .p-0 2.
         .flex-1.text-center PAY WITH STRIPE
         .i-la-check.absolute.right-4(v-if="paid")
-
-      .text-green-600.p-4.bg-shadow-xl.rounded-2xl.text-center.text-xl(v-if="scheduled && paid") All set! See you soon!
+      .text-sm(v-if="!paid") You can pay any time before or after the session.
+      .text-green-600.p-4.bg-shadow-xl.rounded-2xl.text-center.text-xl(v-if="scheduled && paid") All set! See you there!
 
   .flex.flex-wrap.gap-8.items-start(style="flex: 1 1 300px")
     .bg-light-400.p-6.rounded-2xl.shadow-xl.flex.flex-col.gap-4(style="flex: 1 1 300px")
-      p Find my expertise fit to guide your own musical journey? Let's have a talk! I'm ready to share my experience and knowledge in dense and complete form of 1-hour lessons according to personal program developed on the first orientational meeting.
+      p Not sure if my expertise is fit to guide your own musical journey? Let's have a talk! We can discuss your ideas, doubts and aspirations before the session. I'm open for any kind of educational and creative collaboration formats.  
       .flex.flex-wrap.gap-4.w-full
         a.flex.gap-4.items-center.z-4.p-4.rounded-xl.shadow-xl.bg-dark-200.text-light(
           href="https://discordapp.com/users/535370860521979914/" 
@@ -138,11 +142,11 @@ async function pay() {
           .i-la-instagram.text-2xl
           .text-lg chromatone.center
   .flex.flex-wrap.gap-4.w-full.items-start.flex-1
-    button.flex.items-center.gap-4.p-4.bg-dark-200.text-light.shadow-xl.rounded-2xl(v-if="isSupported" @click="startShare()")
+
+    .shadow-xl.rounded-2xl.overflow-clip.z-10.max-w-45c.min-w-40.max-w-45ch(style="flex: 1 1 240px" v-html="svg")
+    button.flex.items-center.gap-4.p-4.bg-dark-200.text-light.shadow-xl.rounded-2xl(style="flex: 1 1 100%" v-if="isSupported" @click="startShare()")
       .i-la-share.text-4xl
       .text-2xl Share this page
-    .shadow-xl.rounded-2xl.overflow-clip.z-10.max-w-45c.min-w-40.max-w-45ch(style="flex: 1 1 240px" v-html="svg")
-
 </template>
 
 <style scoped lang="postcss"></style>
