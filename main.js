5738b6df9a1b478e720eb882c5bf9b9839cb4f91
davay <davay@defucc.me>
2024-06-13 16:37:09 +0700

    fix

--- main.js
+++
@@ -1,8 +0,0 @@
-import '@unocss/reset/tailwind.css'
-import 'virtual:uno.css'
-import './style.postcss'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
